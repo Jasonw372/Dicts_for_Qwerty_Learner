3afbf24ea6c81e50f9d6081cc100a533c6d1517f
huanyushi <1174601344@qq.com>
2023-07-20 18:02:47 +0800

    init

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-
 import pandas as pd
 
 if __name__ == '__main__':
@@ -7,10 +6,10 @@ if __name__ == '__main__':
     dict_path = "dicts"
     export_path = "exports"
     for name in os.listdir(dict_path):
-        print(f"开始处理{name}...")
+        # print(f"开始处理{name}...")
         df = pd.read_csv(os.path.join(dict_path, name), header=None)
         df.columns = ['name', 'trans']
-        print(df.shape)
+        # print(df.shape)
         try:
             df['trans'] = df['trans'].apply(lambda x: [x.replace("\n", ",")])
             df.to_json(os.path.join('exports', name.replace("csv", "json")), orient='records', lines=False,
