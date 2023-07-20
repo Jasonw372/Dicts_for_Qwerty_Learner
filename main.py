dfa544b8aa4401c1d2444d941029351b14c64e13
huanyushi <1174601344@qq.com>
2023-07-20 18:01:26 +0800

    init

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+import pandas as pd
+
+if __name__ == '__main__':
+
+    dict_path = "dicts"
+    export_path = "exports"
+    for name in os.listdir(dict_path):
+        print(f"开始处理{name}...")
+        df = pd.read_csv(os.path.join(dict_path, name), header=None)
+        df.columns = ['name', 'trans']
+        print(df.shape)
+        try:
+            df['trans'] = df['trans'].apply(lambda x: [x.replace("\n", ",")])
+            df.to_json(os.path.join('exports', name.replace("csv", "json")), orient='records', lines=False,
+                       force_ascii=False)
+        except:
+            print(f"处理{name}失败...")
+            continue
+        # print(f"处理{name}完成...")
